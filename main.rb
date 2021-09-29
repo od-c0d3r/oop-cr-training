582b1c21881f464000662a62ef6f4f79c3c2b174
Carlos-SSH <carlos.anezeh@me.com>
2021-09-29 17:44:50 -0500

    Fixed offenses and refactorize a method.

--- main.rb
+++ main.rb
@@ -75,21 +75,22 @@ class App
 
   def create_rental
     puts 'Select a book from the following list by number'
-    @books.each_with_index { |book, index| puts "#{index}) Title: \"#{book.title}\" Author: #{book.author}" }
+    @books.each_with_index do |book, index|
+      puts "#{index}) Title: \"#{book.title}\", Author: #{book.author}"
+    end
     book_index = gets.chomp.to_i
 
     puts "\nSelect a person from the following list by number (not id)"
-
-    @people.each_with_index { |person, index|
+    @people.each_with_index do |person, index|
       puts "#{index}) [#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
-    }
+    end
     person_index = gets.chomp.to_i
 
     print "\nDate: "
     date = gets.chomp
 
-    @rentals << Rental.new(date, @booksp[book_index], @people[person_index])
-    puts 'Rental created successfully!'
+    @rentals << Rental.new(date, @books[book_index], @people[person_index])
+    puts 'Rental created successfully'
   end
 
   def list_rentals_for_person_id
